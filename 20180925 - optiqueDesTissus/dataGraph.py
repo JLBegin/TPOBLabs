64d689e25235710ba3ad988d2e8f4593a74782b2
Ludovick Bégin <ludovick.begin@gmail.com>
2018-10-09 08:45:28 -0400

    cleaned the graph
    not sure if this is really what we want to show

--- 20180925 - optiqueDesTissus/dataGraph.py
+++ 20180925 - optiqueDesTissus/dataGraph.py
@@ -7,10 +7,12 @@ class DataGraph:
         self.rawData = []
         self.normData = []
         self.smoothFactor = 10  # How much values to avg ( > 0 )
+        self.fig, self.axes = plt.subplots(2, figsize=(15, 10))
 
     def compareCorrectedData(self):
         self.getData()
         self.plotData()
+        self.adjustPlot()
         self.show()
 
     def getData(self):
@@ -21,16 +23,32 @@ class DataGraph:
         for e, data in enumerate([self.rawData, self.normData]):
 
             numberOfColumns = data[0].shape[0] - 1
-            timeVector = data[:, 0][::self.smoothFactor]
+            timeVector = data[:, 0][::self.smoothFactor] - 100
 
             for i in range(numberOfColumns):
                 normData = data[:, i+1] / np.mean(data[:, i+1])
                 if self.smoothFactor > 1:
+                    stDev = round(np.std(normData)*100, 2)
+                    normData = (normData - 1) * 100
                     normData = [np.mean(normData[i*self.smoothFactor:(i+1)*self.smoothFactor]) for i in range((len(normData)//self.smoothFactor)+1)]
-                plt.plot(timeVector, normData, label=["Incidence", "Transmitance", "Réflectance"][i], color=["r", "g", "b"][i], linestyle=["--", "-"][e])
+                self.axes[e].plot(timeVector, normData, label="{} ($\sigma$ = {}%)".format(["Incidence", "Transmitance", "Réflectance"][i], stDev), color=["r", "g", "b"][i], linestyle=["-", "-"][e])
+
+    def adjustPlot(self):
+        for axe in self.axes:
+            axe.set_xlim(0, 150)
+            axe.set_ylim(-2, 4)
+            axe.set_ylabel("$\Delta$F/F [%]", fontsize=17)
+            axe.set_xlabel("Time [s]", fontsize=17)
+            axe.legend(loc="upper right", fontsize=15)
+            axe.tick_params(labelsize=16, length=8, width=2)
+            axe.axhline(0, color="black")
+            axe.axhline(-2, color="black", linewidth=3)
+            axe.axhline(4, color="black", linewidth=3)
+            axe.axvline(0, color="black", linewidth=3)
+            axe.axvline(150, color="black", linewidth=3)
+        plt.tight_layout(h_pad=3)
 
     def show(self):
-        plt.legend(loc="best")
         plt.show()
 
 DataGraph().compareCorrectedData()
