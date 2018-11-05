a9d543c5cadccb6dbacfe62e7e03c72bdaa1f1e3
Ludovick Bégin <ludovick.begin@gmail.com>
2018-11-05 14:19:45 -0500

    new figure

--- 20180925 - optiqueDesTissus/dataGraph.py
+++ 20180925 - optiqueDesTissus/dataGraph.py
@@ -6,7 +6,7 @@ class DataGraph:
     def __init__(self):
         self.rawData = []
         self.normData = []
-        self.smoothFactor = 10  # How much values to avg ( > 0 )
+        self.smoothFactor = 3  # How much values to avg ( > 0 )
         self.fig, self.axes = plt.subplots(2, figsize=(15, 10))
 
     def compareCorrectedData(self):
@@ -17,20 +17,30 @@ class DataGraph:
 
     def getData(self):
         self.rawData = np.loadtxt("./data/readingRaw300sec1.txt")
-        self.normData = np.loadtxt("./data/readingNorm300sec.txt")
+        self.normData = np.loadtxt("./data/readingNorm300sec.txt")[155:850]
 
     def plotData(self):
         for e, data in enumerate([self.normData, self.normData]):  # Fastest and ugliest data back correction
             numberOfColumns = data[0].shape[0] - 1
-            timeVector = data[:, 0][::self.smoothFactor] - 100
+            timeVector = data[:, 0][::self.smoothFactor] - 13
 
             for i in range(numberOfColumns):
                 normData = data[:, i+1]
-                if e == 0 and i > 0:
-                    rawFactor = data[:, 1] / data[:, 1][0]
+                normData /= np.mean(normData)
+
+                if e == 0 and i > 0:  # From corrected data to raw data
+                    rawFactor = data[:, 1] / np.mean(data[:, 1])
                     normData = data[:, i+1] * rawFactor
 
-                normData /= np.mean(normData)
+                if e == 1 and i > 0:
+                    rawFactor = data[:, 1] / np.mean(data[:, 1])
+                    normData = data[:, i+1] * rawFactor
+
+                    conversionFactor = [1.2, 1.1]
+                    normData /= np.mean(rawFactor) + ((rawFactor - np.mean(rawFactor)) * conversionFactor[i-1])
+
+                    print(np.mean(normData), np.mean(rawFactor))
+
                 stDev = round(np.std(normData)*100, 2)
                 if self.smoothFactor > 1:
                     normData = (normData - 1) * 100
@@ -39,7 +49,7 @@ class DataGraph:
 
     def adjustPlot(self):
         for axe in self.axes:
-            axe.set_xlim(0, 150)
+            axe.set_xlim(0, 54)
             axe.set_ylim(-2, 4)
             axe.set_ylabel("Intensité relative", fontsize=17)
             axe.set_xlabel("Temps [s]", fontsize=17)
