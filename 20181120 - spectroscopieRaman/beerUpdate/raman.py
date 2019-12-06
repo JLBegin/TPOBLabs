904b4e6b02aa0aa64d3f8ac69562d1cc76aa2507
JLBegin <ludovick.begin@gmail.com>
2019-12-06 10:58:25 -0500

    update Raman code with an alternative code for beer spectra

--- 20181120 - spectroscopieRaman/beerUpdate/raman.py
+++ 20181120 - spectroscopieRaman/beerUpdate/raman.py
@@ -1,5 +1,11 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+Spin-off of main graphRaman.py file.
+Used to load pre-calibrated files of wavelength (in descending order) & intensity
+*The cut window and the graphs have not yet been optimized for the beer data.
+"""
 
 
 class Raman:
@@ -7,37 +13,32 @@ class Raman:
         self.fitPixels = True
         self.removeFluo = True
 
-        self.oilFiles = ["data/{}.TXT".format(oil) for oil in ["mais", "arachide", "tournesol", "canola", "olive40m"]]
-        self.oilNames = ["Maïs", "Arachide", "Tournesol", "Canola", "Olive"]
-        self.integrationTimes = [100, 100, 100, 100, 4500]
+        self.beerFiles = ["data/{}.txt".format(beerName) for beerName in ["tremblay", "coors"]]
+        self.beerNames = ["Tremblay", "Coors"]
+        self.integrationTimes = [150, 100]
 
-        self.solFiles = ["data/{}.TXT".format(solution) for solution in ["ethanol", "isopropanol", "methanol", "glycerol", "sucrose"]]
-        self.solNames = ["Éthanol", "Isopropanol", "Méthanol", "Glycérol", "Sucrose"]
-
-        self.readNoiseValue = 61634
-        self.thermalCoef = 8.97
+        self.readNoiseValue = 62825
+        self.thermalCoef = 1.683
         self.photonsPerBit = 4.35
 
         self.calibCoefs = []
-        self.pixels = []
+        self.wavelengths = []
         self.waveNumbers = []
         self.intensities = []
 
         self.fileName = ""
 
-    def graphOils(self):
-        fig, axes = plt.subplots(len(self.oilFiles), sharex=True, sharey=False)
+    def graphBeer(self):
+        fig, axes = plt.subplots(len(self.beerFiles), sharex=True, sharey=False)
 
-        self.setCalibration()
-
-        for i, file in enumerate(self.oilFiles):
-            pixel, intensity = self.getData(file)
+        for i, file in enumerate(self.beerFiles):
+            wavelength, intensity = self.getData(file)
             intensity -= self.readNoiseValue
             intensity -= self.thermalCoef * self.integrationTimes[i]
             intensity *= self.photonsPerBit
             intensity /= self.integrationTimes[i]
 
-            waveNumber = self.translate(pixel)
+            waveNumber = self.translate(wavelength)
 
             waveNumber, intensity = self.cut(waveNumber, intensity)
 
@@ -46,7 +47,7 @@ class Raman:
             raman = intensity - fit
 
             axes[i].plot(waveNumber, intensity)
-            axes[i].plot(waveNumber, raman, label=self.oilNames[i])
+            axes[i].plot(waveNumber, raman, label=self.beerNames[i])
             axes[i].legend(handlelength=0, fontsize=12)
             axes[i].set_ylim(min(raman), 3200)
             axes[i].tick_params(labelsize=12)
@@ -91,71 +92,24 @@ class Raman:
 
         plt.show()
 
-    def graphSols(self):
-        fig, axes = plt.subplots(len(self.solFiles), sharex=True, sharey=False)
-
-        self.setCalibration()
-
-        for i, file in enumerate(self.solFiles):
-            pixel, intensity = self.getData(file)
-            intensity -= self.readNoiseValue
-            intensity -= self.thermalCoef * 100
-
-            waveNumber = self.translate(pixel)
-
-            waveNumber, intensity = self.cut(waveNumber, intensity, low=250, high=1600)
-
-            fit = 0
-            if i == 3 or 4:
-                fit = self.curveFit(waveNumber, intensity, degree=2, sections=1)
-
-            raman = intensity - fit
-            raman /= 100
-            raman *= self.photonsPerBit
-            axes[i].plot(waveNumber, raman, label=self.solNames[i])
-            axes[i].legend(handlelength=0, fontsize=12)
-            axes[i].set_ylim(min(raman), 4000)
-            axes[i].tick_params(labelsize=12)
-
-            if i == 2:
-                axes[i].set_ylabel("Intensité [p/s]", fontsize=13)
-
-        plt.xlabel("Nombre d'onde [cm$^{-1}$]", fontsize=13)
-        plt.xlim(250, 1600)
-        fig.subplots_adjust(hspace=0, top=0.965, bottom=0.11, left=0.14, right=0.94, wspace=0.2)
-
-        plt.show()
-
     def getFile(self):
-        self.oilFiles = ["data/{}.TXT".format(oil) for oil in ["mais", "arachide", "tournesol", "canola", "olive40m"]]
+        self.beerFiles = ["data/{}.txt".format(beer) for beer in ["tremblay", "coors"]]
 
         self.fileName = "data/{}".format(["mercury_27nov.txt", "ethanol.TXT", "olive40m.TXT", "canola.TXT", "mais.TXT",
                                           "arachide.TXT", "tournesol.TXT", "lampeMercure1.TXT"][6])
 
     @staticmethod
     def getData(fileName):
-        pixels = []
+        wavelengths = []
         intensities = []
         with open(fileName, "r") as file:
-            for line in file.readlines()[:-1]:
-                data = line.split(",")[1:]
-                pixels.append(float(data[0]))
+            for line in reversed(file.readlines()[:-1]):
+                data = line.split()
+                wavelengths.append(float(data[0]))
                 intensities.append(float(data[1]))
-        return np.array(pixels), np.array(intensities)
+        return np.array(wavelengths), np.array(intensities)
 
-    def setCalibration(self):
-        mercure1 = [[588, 842.2, 1089.5, 1104.3], [671, 690, 708, 709]]
-        mercure2 = [[587.5, 841, 1089, 1103.5], [671, 690, 708, 709]]
-        # ethanol = [[571.2, 670.3, 697, 809.2, 927.2], self.wToLambda([879, 1050, 1085, 1279, 1450])]
-
-        pixels = mercure1[0] + mercure2[0]
-        wavelengths = mercure1[1] + mercure2[1]
-
-        self.calibCoefs = np.polyfit(pixels, wavelengths, 1)
-
-    def translate(self, pixels):
-        [a, b] = self.calibCoefs
-        wavelengths = np.array(pixels)*a + b
+    def translate(self, wavelengths):
         waveNumbers = 1/632.8 - 1/wavelengths
         return waveNumbers * 10**7
 
@@ -166,7 +120,6 @@ class Raman:
     def cut(self, waveNumbers, intensities, low=1200, high=1850):
         start = np.where(waveNumbers > low)[0][0]
         end = np.where(waveNumbers > high)[0][0]
-
         return waveNumbers[start: end], intensities[start: end]
 
     @staticmethod
@@ -187,5 +140,4 @@ class Raman:
         np.savetxt("outputRaman.txt", data, '%.15f')
 
 
-Raman().graphOils()
-Raman().graphSols()
+Raman().graphBeer()
